79224f743ed8e2557d781c090965ac88fde42909
emeric <emeric254@live.fr>
2015-09-16 21:23:38 +0200

    ajout ldap client

---
+++ ldap-client.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 4 ]
+then
+    echo "Usage: ./$0 <ip> <ou> <dc> <dc> "
+    exit -1
+fi
+
+ip=$1
+organisation=$2
+dc1=$3
+dc2=$4
+
+aptitude install ldap-utils
+aptitude install libnss-ldap
+
+#~ ldapsearch -LLL -H ldap://$ip \ -b dc=$dc1 -D cn=admin,dc=$dc1 -W uid=robin
+#~ --Changer le mdp de robin
+#~ ldappasswd -x -H ldap://$ip -D cn=admin,dc=$dc1 -W -S uid=robin,ou=people,dc=$dc1
+
