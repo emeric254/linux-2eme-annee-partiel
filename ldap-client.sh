8a13398495b259cf2ef9e09e0b11d73597d3a6d4
emeric <emeric254@live.fr>
2015-09-16 21:42:34 +0200

    avancement de ldap-client.sh

--- ldap-client.sh
+++ ldap-client.sh
@@ -11,8 +11,30 @@ organisation=$2
 dc1=$3
 dc2=$4
 
-aptitude install ldap-utils
-aptitude install libnss-ldap
+apt-get install ldap-utils
+apt-get install libnss-ldap libpam-ldap nscd
+
+#~ Pour le paquet libpam-ldap, voici la liste des options retenues.
+    #~ Identifiant uniforme de ressource (« URI ») d'accès au serveur LDAP : ldap://$ip
+    #~ Nom distinctif (DN) de la base de recherche : dc=$dc2,dc=$dc1
+    #~ Version de LDAP à utiliser : 3
+    #~ Donner les privilèges de superutilisateur local au compte administrateur LDAP ? oui
+    #~ La base de données LDAP demande-t-elle une identification ? non
+    #~ Compte de l'administrateur LDAP : cn=admin,dc=$dc2,dc=$dc1
+    #~ Mot de passe du compte de l'administrateur LDAP : *****
+    #~ Algorithme de chiffrement à utiliser localement pour les mots de passe : Chiffré
+    #~ Profils PAM à activer : Unix authentication + LDAP Authentication
+echo "Menu de configuration du paquet libnss-ldap"
+dpkg-reconfigure libnss-ldap
+echo "Menu de configuration du paquet libpam-ldap"
+dpkg-reconfigure libpam-ldap
+
+echo "configuration de /etc/nsswitch.conf"
+sed -i 's/passwd:/passwd:         compat ldap/g' /etc/nsswitch.conf
+sed -i 's/group:/group:         compat ldap/g' /etc/nsswitch.conf
+sed -i 's/shadow:/shadow:         compat ldap/g' /etc/nsswitch.conf
+echo "[OK]"
+
 
 #~ ldapsearch -LLL -H ldap://$ip \ -b dc=$dc1 -D cn=admin,dc=$dc1 -W uid=robin
 #~ --Changer le mdp de robin
