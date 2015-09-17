ec2684e9a759f72b54fa38c0dc28b949a87bd319
emeric <emeric254@live.fr>
2015-09-17 20:42:52 +0200

    ajout comments ldap-client pour la config des paquets

--- ldap-client.sh
+++ ldap-client.sh
@@ -20,6 +20,33 @@ ldapsearch -LLL -H ldap://$ip -b dc=$dc2,dc=$dc1 -D cn=admin,dc=$dc2,dc=$dc1 -W 
 echo "[OK]"
 
 
+
+
+#~  pour un appel a ce script avec  # ./$0 <ip server> < orga > <dc2> <dc1>
+#~          c'est a dire ou=orga et dc=dc2.dc1
+#~
+#~ >>> libnss
+    #~ l'uri du serveur : ldap://$ip
+    #~ distinguished name of search base : dc=$dc2,$dc=$dc1
+    #~ LDAP version : 3
+    #~ does the ldap database require login : no
+    #~ special LDAP privileges for root : yes
+    #~ make the configuration file readable....: no
+    #~ LDAP accound for root : cn=admin,dc=$dc1,dc=$dc2
+    #~ ldap password : j'ai besoin de le dire
+    #~ nsswitch.conf not managed automatically : ba tu fait juste ok
+#~
+#~ >>> libpam
+    #~ LDAP server URI : ldap://$ip
+    #~ distinguished name of the search base : dc=$dc2,dc=$dc1
+    #~ LDAP version to use : 3
+    #~ allow LDAP admin account to behave like local root ? yes
+    #~ does the LDAP database require login : no
+    #~ LDAP administrative account : cn=admin,dc=$dc1,dc=$dc2
+    #~ LDAP administrative password : ba le mot de passe
+    #~ local encryption algorithm to use for passwords : crypt
+    #~ PAM ofiles to enable : Unix et LDAP
+#~
 #~ Pour le paquet libpam-ldap, voici la liste des options retenues.
     #~ Identifiant uniforme de ressource (« URI ») d'accès au serveur LDAP : ldap://$ip
     #~ Nom distinctif (DN) de la base de recherche : dc=$dc2,dc=$dc1
@@ -30,6 +57,8 @@ echo "[OK]"
     #~ Mot de passe du compte de l'administrateur LDAP : *****
     #~ Algorithme de chiffrement à utiliser localement pour les mots de passe : Chiffré
     #~ Profils PAM à activer : Unix authentication + LDAP Authentication
+
+
 echo "Menu de configuration du paquet libnss-ldap"
 dpkg-reconfigure libnss-ldap
 echo "Menu de configuration du paquet libpam-ldap"
