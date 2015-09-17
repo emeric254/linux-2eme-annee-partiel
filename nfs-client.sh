872912381971feae301cfdd2b2705870355ff44a
emeric254 <emeric254@live.fr>
2015-09-17 16:00:05 +0200

    debut nfs

--- nfs-client.sh
+++ nfs-client.sh
@@ -1,2 +1,51 @@
 #!/bin/bash
 
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: ./$0 <ip> "
+    exit -1
+fi
+
+ip=$1
+
+#~ installation des paquets
+apt-get install rpcbind nfs-common
+
+#~ config nfs-common
+sed -i "s/NEED_STATD=/NEED_STATD=no/g" /etc/default/nfs-common
+sed -i "s/NEED_IDMAPD=/NEED_IDMAPD=YES/g" /etc/default/nfs-common
+
+#~ on arrete le deamon
+#~ /etc/init.d/nfs-common stop
+service nfs-common stop
+killall rpc_statd
+
+#~ on demare le deamon
+#~ /etc/init.d/nfs-common start
+service nfs-common start
+
+#~ quelques tests
+#~ rpcinfo -s $ip
+#~ showmount -e $ip
+#~ dpkg -S `which mount`
+
+#~ creation du repertoire qui fera dossier partagé
+mkdir /ahome
+mount -t nfsv4 $ip:/home /ahome
+
+#~ rendre le montage MANUELLEMENT permanent:
+echo "$ip:/home \t /ahome \t nfs4 \t 0 \t 0" >> /etc/ftab
+
+#~ ---------------------------------------------------------------------
+
+#~ installation de auto-fs
+apt-get install autofs
+
+#~ creation d'un utilisateur avec home dans le dossier partagé
+adduser --no-create-home --home /ahome/etu-nfs etu-nfs
+
+#~ ajouter /ahome \t /etc/auto.home à /etc/auto.master
+cat <<EOF >> /etc/auto.home
+*       -fstype=nfs4    $ip:/home/&
+EOF
--- nfs-server.sh
+++ nfs-server.sh
@@ -1,2 +1,47 @@
 #!/bin/bash
 
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: ./$0 <ip> <masque> "
+    exit -1
+fi
+
+ip=$1
+masque=$2
+
+#~ installation des paquets
+apt-get install rpcbind nfs-common nfs-kernel-server
+
+#~ config nfs-common
+sed -i "s/NEED_STATD=/NEED_STATD=no/g" /etc/default/nfs-common
+sed -i "s/NEED_IDMAPD=/NEED_IDMAPD=YES/g" /etc/default/nfs-common
+
+#~ on arrete le deamon
+#~ /etc/init.d/nfs-common stop
+service nfs-common stop
+killall rpc_statd
+
+#~ on demare le daemon
+#~ /etc/init.d/nfs-common start
+service nfs-common start
+
+#~ .
+#~ scp du fichier exports
+#~ .
+
+#~ /etc/init.d/nfs-kernel-server restart
+service nfs-kernel-server restart
+
+#~ creation du dossier ahome partagé
+mkdir /ahome
+mount --bind /home/exports/home /ahome
+
+#~ creation fichier exports
+cat <<EOF >> /etc/exports
+/home/exports           $ip/$masque(rw,sync,fsid=0,crossmnt,no_subtree_check)
+/home/exports/home      $ip/$masque(rw,sync,no_subtree_check)
+EOF
+
+#~ ajout utilisateur local avec home dans le dossier de partage
+adduser --home /ahome/etu-nfs etu-nfs
