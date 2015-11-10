1041476969ad23df0aaab7205111198bc62922b7
emeric254 <emeric254@live.fr>
2015-11-10 13:48:50 +0100

    preparatif

---
+++ preparatifs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+nodeName=`uname -n`
+
+# installation des paquets
+echo "installation des paquets"
+apt-get update
+apt-get install ipppd apache2 quagga
+
+
+# affichage details pour le poste
+echo "details réseau pour ce poste : $nodeName"
+echo "	- RNIS : "
+grep $nodeName table-addr-RNIS
+echo "	- VLAN : "
+grep $nodeName table-addr-VLAN
+
+
+# activation ip foward
+echo "activation ip_forward ipv4"
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# iptables -t nat -A POSTROUTING -o $interfaceSortie -j MASQUERADE
+
+
+# virer le dhclient qui reset la conf toutes les 5 mins
+echo "killall dhclient"
+killall dhclient
