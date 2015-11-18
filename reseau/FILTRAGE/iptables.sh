e3ab137258960ed8f28036a01437e2dd5d0b21c1
emeric254 <emeric254@live.fr>
2015-11-18 22:35:34 +0100

    wip

---
+++ reseau/FILTRAGE/iptables.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# etat iptables table filtrage
+iptables -t filter -L
+
+# etat iptables table nat
+iptables -t nat -L
+
+# tout les paquets seront drop en entree
+iptables -P INPUT DROP
+
+# tout les paquets seront drop en forward
+iptables -P FORWARD DROP
+
+# tout les paquets seront drop en sortie
+iptables -P OUTPUT DROP
+
+# autoriser le trafic deja etabli
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# c'est mieux de garder l'interface de loopback dispo pour les process locaux sinon c'est un peu du suicide xD
+iptables -A INPUT -i lo -m conntrack --ctstate NEW -j ACCEPT
+iptables -A OUTPUT -o lo -m conntrack --ctstate NEW -j ACCEPT
+
+# on sauvegarde la conf actuelle
+echo "sauvegarde de la configuration actuelle dans le fichier ./iptables.conf"
+iptables-save > iptables.conf
+
+# configuration a la main
+echo ">> iptables.conf"
+echo "finissez la configuration a la main"
+sleep 2
+vim iptables.conf
+
+# on restaure la conf
+echo "on va restaurer la configuration"
+sleep 1
+echo "restauration ..."
+iptables-restore iptables.conf
+echo "voila !"
+
